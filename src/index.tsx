2b5d14e12819190317e24df6a2aa1696f3c64820
pavlov_roman <pavro.roman@gmail.com>
2025-02-02 20:07:16 +0300

    Добавлены alias в конфиг

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
-import { StoreProvider } from 'app/providers/StoreProvider';
+import { StoreProvider } from '@/app/providers/StoreProvider';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
