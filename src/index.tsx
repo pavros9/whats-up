5ffcb8a6e3ce321653575edba9af6c9107e5403f
pavlov_roman <pavro.roman@gmail.com>
2025-02-02 23:15:49 +0300

    Добалвлен провайдер для переменных темы в antd

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import { StoreProvider } from '@/app/providers/StoreProvider';
+import { ConfigProvider } from 'antd'
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement,
 );
 root.render(
     <StoreProvider>
-        <App />
+        <ConfigProvider
+            theme={{
+                token: {
+                    colorPrimary: '#00b96b',
+                    borderRadius: 2,
+                    colorBgContainer: '#ffffff',
+                },
+            }}
+        >
+            <App />
+        </ConfigProvider>
     </StoreProvider>,
 );
